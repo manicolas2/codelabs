9b22e958f461004e92f1bda8623ad1e65ea9f9ad
manicolas2 <manicolas2@up.edu.ph>
2018-06-06 17:56:01 +0800

    Added function to filter data, fix #32

--- friendlyeats-web/scripts/FriendlyEats.Data.js
+++ friendlyeats-web/scripts/FriendlyEats.Data.js
@@ -60,6 +60,27 @@ FriendlyEats.prototype.getFilteredRestaurants = function(filters, render) {
   /*
     TODO: Retrieve filtered list of restaurants
   */
+  var query = firebase.firestore().collection('restaurants');
+
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+  
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+  
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price.length);
+  }
+  
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRatings', 'desc');
+  }
+ 
+  this.getDocumentsInQuery(query, render);
 };
 
 FriendlyEats.prototype.addRating = function(restaurantID, rating) {
